e239b9f470aa3f56500dd5653ae3642cdc464dd2
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-06 19:51:58 -0300

    treinando tipo de valor das variaveis

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,25 @@
-﻿string titular = "Igor Neves";
-string conta = "254935-x";
-string numero_agencia = "23";
-string nome_agencia = "Agencia Central";
-double saldo = 105.98;
+﻿using ByteBank;
 
-Console.WriteLine("Titular: " + titular);
-Console.WriteLine("Conta: " + conta);
-Console.WriteLine("Número da agencia: " + numero_agencia);
-Console.WriteLine("Nome da agência: " + nome_agencia);
-Console.WriteLine("Saldo: " + saldo);
+ContaCorrente conta1 = new ContaCorrente();
+
+conta1.titular = "Igor Neves";
+conta1.conta = "251336 - x";
+conta1.saldo = 105.98;
+conta1.numero_agencia = 23;
+conta1.nome_agencia = "Agência Central";
+
+ContaCorrente conta2 = new ContaCorrente();
+
+Console.WriteLine("Titular: " + conta2.titular);
+Console.WriteLine("Conta: " + conta2.conta);
+Console.WriteLine("Número da agencia: " + conta2.numero_agencia);
+Console.WriteLine("Nome da agência: " + conta2.nome_agencia);
+Console.WriteLine("Saldo: " + conta2.saldo);
+
+double saldo = 100;
+double saldo2 = conta1.saldo;
+Console.WriteLine(saldo == conta1.saldo);
+Console.WriteLine(saldo != saldo2);
 
 Console.ReadKey();
 
