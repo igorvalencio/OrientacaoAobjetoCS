6308c97519df59e585eb80df1db5c35a0c7a79ce
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-06 18:48:40 -0300

    Aprendendo orientação a objetos

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string titular = "Igor Neves";
+string conta = "254935-x";
+string numero_agencia = "23";
+string nome_agencia = "Agencia Central";
+double saldo = 105.98;
+
+Console.WriteLine("Titular: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Número da agencia: " + numero_agencia);
+Console.WriteLine("Nome da agência: " + nome_agencia);
+Console.WriteLine("Saldo: " + saldo);
+
+Console.ReadKey();
+
